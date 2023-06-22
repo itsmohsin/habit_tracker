7d96c85408098f7af8621f00ad6a226d25bb091c
Mohsin Khan <mohsinsmannat@gmail.com>
2023-06-22 16:46:08 +0530

    donw with creating new habit

---
+++ lib/components/new_habit_box.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class EnterNewHabitBox extends StatelessWidget {
+  final controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  const EnterNewHabitBox(
+      {super.key,
+      this.controller,
+      required this.onSave,
+      required this.onCancel});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.grey[900],
+      content: TextField(
+        controller: controller,
+        style: const TextStyle(color: Colors.white),
+        decoration: const InputDecoration(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        ),
+      ),
+      actions: [
+        MaterialButton(
+          onPressed: onSave,
+          color: Colors.black,
+          child: const Text(
+            "Save",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        MaterialButton(
+          onPressed: onCancel,
+          color: Colors.black,
+          child: const Text(
+            "Cancel",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/components/habit_tile.dart';
+import 'package:habit_tracker/components/my_fab.dart';
+import 'package:habit_tracker/components/new_habit_box.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +11,74 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // data structure for todays list
+  List todaysHabitList = [
+    //[habitName, habitCompleted]
+    ['Morning Run', false],
+    ['Read Book', false],
+    ['Code App', false],
+  ];
+
+  //checkbox was tapped
+  void checkBoxTapped(bool? value, int index) {
+    setState(() {
+      todaysHabitList[index][1] = value;
+    });
+  }
+
+  // create a new habit
+  final _newHabitNameController = TextEditingController();
+
+  void createNewHabit() {
+    // show alert dialog for user to enter the new habit details
+    showDialog(
+        context: context,
+        builder: (context) {
+          return EnterNewHabitBox(
+            controller: _newHabitNameController,
+            onSave: saveNewHabit,
+            onCancel: cancelNewHabit,
+          );
+        });
+  }
+
+  // save new habit
+  void saveNewHabit() {
+    // add new habit to todays habit list
+    setState(() {
+      todaysHabitList.add([_newHabitNameController.text, false]);
+    });
+    // clear textfield
+    _newHabitNameController.clear();
+    // pop dialog box
+    Navigator.of(context).pop;
+  }
+
+  // cancel new habit
+  void cancelNewHabit() {
+    // clear textfield
+    _newHabitNameController.clear();
+    // pop dialog box
+    Navigator.of(context).pop;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      floatingActionButton: MyFloatingActionButton(
+        onPressed: () => createNewHabit(),
+      ),
+      body: ListView.builder(
+        itemCount: todaysHabitList.length,
+        itemBuilder: (context, index) {
+          return HabitTile(
+            habitName: todaysHabitList[index][0],
+            habitCompleted: todaysHabitList[index][1],
+            onChanged: (value) => checkBoxTapped(value, index),
+          );
+        },
+      ),
     );
   }
 }
